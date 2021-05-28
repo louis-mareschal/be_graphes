ff9429b18aa4e8070d7a0d5ebe1135cb9fa851e9
Louis Mareschal <marescha@insat.insa-toulouse.fr>
2021-05-28 09:32:57 +0200

    Dernier commit

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/LabelVelo.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/LabelVelo.java
@@ -2,17 +2,16 @@ package org.insa.graphs.algorithm.shortestpath;
 import org.insa.graphs.model.*;
 public class LabelVelo extends Label implements Comparable<Label> {
 	
-	private int coeff;
+	
 
 
 	
     public LabelVelo(Node node, boolean mark, float cost, Arc father, ShortestPathData data) {
         super(node, mark, cost, father); 
-        this.coeff = 1;
         
     }
     public float getTotalCost() {
- 	    return this.getCost() * this.coeff;
+ 	    return this.getCost();
     }
     
 }   
--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/ProjetVeloAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/ProjetVeloAlgorithm.java
@@ -1,8 +1,12 @@
 package org.insa.graphs.algorithm.shortestpath;
 
+import java.util.EnumSet;
+
 import org.insa.graphs.algorithm.AbstractInputData;
 import org.insa.graphs.model.Arc;
 import org.insa.graphs.model.Node;
+import org.insa.graphs.model.AccessRestrictions.AccessMode;
+import org.insa.graphs.model.AccessRestrictions.AccessRestriction;
 import org.insa.graphs.model.RoadInformation.RoadType;
 
 public class ProjetVeloAlgorithm extends DijkstraAlgorithm {
@@ -16,16 +20,33 @@ public class ProjetVeloAlgorithm extends DijkstraAlgorithm {
     }
     @Override
     protected float getCostArc(Arc arc) {
-    	float vitesse = (float)arc.getMinimumTravelTime() * 1000 / 3600;
-    	int coeff = 100;
-    	if (arc.getRoadInformation().getType() != RoadType.MOTORWAY) {
-        	coeff = 1000;	
-        }else if (arc.getRoadInformation().getType() != RoadType.CYCLEWAY) {
+    	float vitesse = (float)(15 * 1000 / 3600); // On prend 15 km/h de moyenne
+    	int coeff = 5;
+    	float cost = 0;
+    	float vitesse_arc = (float)arc.getRoadInformation().getMaximumSpeed();
+    	if (arc.getRoadInformation().getType() == RoadType.MOTORWAY) {
+    		if (vitesse_arc >= 110) {
+    			coeff = 50;
+    		} else if (vitesse_arc >= 80.0) {
+    			coeff = 5;
+    		} else if (vitesse_arc >= 50.0) {
+    			coeff = 3;
+    		} else if (vitesse_arc >= 30.0) {
+    			coeff = 2;
+    		}else {
+    			coeff = 1;
+    		}
+        }else if (arc.getRoadInformation().getType() == RoadType.CYCLEWAY || arc.getRoadInformation()
+        		.getAccessRestrictions().isAllowedForAny(AccessMode.FOOT, EnumSet.complementOf(EnumSet
+                .of(AccessRestriction.FORBIDDEN, AccessRestriction.PRIVATE)))) {
         	coeff = 1;
         }
         if (this.data.getMode() != AbstractInputData.Mode.LENGTH) {
         	coeff /= vitesse;
+        	cost = arc.getLength();
+        } else {
+        	cost = arc.getLength()/vitesse;
         }
-    	return (float) data.getCost(arc) * coeff;
+    	return cost * (float)coeff;
     }
 }
